a4f0b344f9b2f2a4b0903e7a61103e3074588c1b
Elsis-Sitati <sitatielsis@gmail.com>
2016-03-30 17:32:59 +0300

    added jquery to image holder

--- foundation-6/js/app.js
+++ foundation-6/js/app.js
@@ -1,2 +1,13 @@
 $(document).ready();
+$("#imageHolder").vegas({
+    
+    cover : true,
+    
+    slides: [
+        { src: "img/images1.jpg" },
+        { src: "img/images2.jpg" },
+        { src: "img/images3.jpg" },
+        { src: "img/images4.jpg" }
+    ]
+});
 
