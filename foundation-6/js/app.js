c4aa2c2cdff91a4198a0c93e69f9754556ec7fea
Elsis-Sitati <sitatielsis@gmail.com>
2016-03-30 17:26:03 +0300

    added and styled imageHolder div

--- foundation-6/js/app.js
+++ foundation-6/js/app.js
@@ -1,9 +1,2 @@
 $(document).ready();
-$("#mainPage").vegas({
-    slides: [
-        { src: "img/images1.jpg" },
-        { src: "img/images2.jpg" },
-        { src: "img/images3.jpg" },
-        { src: "img/images4.jpg" }
-    ]
-});
+
