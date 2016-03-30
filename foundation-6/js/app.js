7d8a50cd4b461d19f472b1fe9a30048411438615
Elsis-Sitati <sitatielsis@gmail.com>
2016-03-30 16:48:37 +0300

    changed images

--- foundation-6/js/app.js
+++ foundation-6/js/app.js
@@ -1,1 +1,9 @@
-$(document).foundation();
+$(document).ready();
+$("#mainPage").vegas({
+    slides: [
+        { src: "img/images1.jpg" },
+        { src: "img/images2.jpg" },
+        { src: "img/images3.jpg" },
+        { src: "img/images4.jpg" }
+    ]
+});
