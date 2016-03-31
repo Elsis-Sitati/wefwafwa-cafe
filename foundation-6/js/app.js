7eeaa37871e3e130bc43bf3c8e97a442d48d8711
Elsis-Sitati <sitatielsis@gmail.com>
2016-03-31 19:52:10 +0300

    added jquery for smooth scroll

--- foundation-6/js/app.js
+++ foundation-6/js/app.js
@@ -10,4 +10,17 @@ $("#imageHolder").vegas({
         { src: "img/images4.jpg" }
     ]
 });
+$(document).ready(function(){
+	$('a[href^="#"]').on('click',function (e) {
+	    e.preventDefault();
 
+	    var target = this.hash;
+	    var $target = $(target);
+
+	    $('html, body').stop().animate({
+	        'scrollTop': $target.offset().top
+	    }, 900, 'swing', function () {
+	        window.location.hash = target;
+	    });
+	});
+});
